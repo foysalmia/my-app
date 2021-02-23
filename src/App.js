b78b2455bfe15e11ac4300da147a05b563fa7e38
Foysal Mia <mfoysal314@gmail.com>
2021-02-24 01:36:52 +0600

    core concept exposed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+let Nayok = ["Dr Mahfuzur Rahman","Alomgir","Bappa Raj","Shakib"];
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit my react < code > src / App.js < /code> and save to reload. < /
-        p > <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> < /
-        header > <
-        /div>
+    return ( 
+        <div className = "App" >
+        <header className = "App-header">
+            <p>I am a react developer</p>
+            <AddPeople></AddPeople>
+
+        </header>
+        </div>
     );
+}
+
+function AddPeople(props){
+    const [count,setCount] = useState(10);
+    const inCrease = () => setCount (count + 1);
+    const decrease = () => setCount (count - 1);
+    return (
+        <div style={{border:"2px solid grey", padding:"30px", margin:"10px"}}>
+            <h4>count:{count}</h4>
+            <button onMouseMove={inCrease}>Increase</button>
+            <button onMouseMove={decrease}>Decrease</button>
+        </div>
+    )
 }
 
 export default App;
